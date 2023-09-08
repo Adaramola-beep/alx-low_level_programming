56c01578addfef0aacb97f3f87a579201149f7b3
Adaramola-beep <tosin_adaramola@yahoo.com>
2023-09-07 20:55:58 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Using a main printf this time
+ * return:0(success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
+}
